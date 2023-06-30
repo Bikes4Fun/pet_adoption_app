1ac952f6c959fab6d4e36a789799f86e8678fe2e
Adam Desatoff <ad.desatoff@gmail.com>
2023-06-30 12:55:47 -0600

    Sepereated front and backend

---
+++ frontend/app.js
@@ -0,0 +1,129 @@
+//making changes
+Vue.createApp({
+    data() {
+        return {
+            pets: [{
+                name: "Cute Pet", 
+                species: "pet species", 
+                breed: "nice breed", 
+                age: "0", 
+                gender: "animal",
+            }], 
+            search: "",
+            filteredPets: [],
+            newPet: {
+                name: "", 
+                species: "", 
+                breed: "", 
+                age: "", 
+                gender: "",
+            },
+            applicants: [],
+            newApplicant: {
+                name: "", 
+                phoneNumber: "",
+                email: "",
+                petId: "",
+            }
+        } //return close
+
+    },//data close
+
+    methods : {
+        //makes a GET request to the server for all pet listings
+        getListings: function(){
+            fetch("https.pet...")
+            .then(response => response.json())
+            .then(data => {
+                this.pets = data;
+                console.log(this.pets);
+            });
+        },
+
+        //makes a POST request to the server from a "create listing" form
+        createListing: function(){
+            //question: I dont understand headers
+
+            myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+            var encodedData = "name=" + encodeURIComponent(this.modal.name) + 
+                                "&species=" + encodeURIComponent(this.modal.species) + 
+                                "&breed=" + encodeURIComponent(this.modal.breed);
+                                //age: "", gender: "",
+            console.log(encodedData);
+
+            var requestOptions = {
+                method: "PUT",
+                body: encodedData,
+                headers: myHeaders
+            };
+            
+            fetch("http://host:pets....", requestOptions)
+            .then((response) => {
+                if (response.status === 201) {
+                    response.json().then((data) => {
+                        this.pets.push(data);
+                        this.newPet = {};
+                    })
+                }
+                else{
+                    alert("Not able to add a pet... ")
+                }
+            })
+        },
+
+        //makes a DELETE request to the server based on the ID number of the pet being deleted
+        deleteListing: function(listingId){
+            var delPet = this.pets[listingId]._id;
+            var requestOptions = {
+                method: "DELETE"
+            };
+            fetch(`http://host/pets/${delId}`, requestOptions)
+                .then((response) => {
+                    if (response.status === 204) {
+                        console.log("pet was deleted");
+                    } else {
+                        alert("pet was not deleted")
+                    }
+                }
+            )
+        },//deleteListing close
+
+        resetSearch: function() {
+            this.search = ""
+        },
+
+        //makes a GET request for all adoption applications
+        getApplications: function(){
+
+        },
+
+        //makes a POST request to the server from a "new adoption" application form
+        createApplication: function(){
+
+        },
+
+        //goes to a different "page" - a.k.a. changes a page data property that hides and shows specific sections
+        changePage: function(page){
+
+        },
+
+    },//methods close
+
+    created : function() {
+        this.getListings();
+    }, //created close
+
+    watch: {
+        search(newSearch, oldSearch) {
+            this.filteredPets = this.pets.filter((pet) => {
+                return pet.name.toLowerCase().includes(newSearch.toLowerCase());
+            });
+        } //search close
+    },//watch close
+
+    computed: {
+    },//computed close
+
+}).mount("#app");
